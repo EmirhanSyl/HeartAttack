a96cc9943f338510b9b2b5dbe1c4cc71e1797e89
EmirhanSyl <61618968+EmirhanSyl@users.noreply.github.com>
2022-02-19 16:29:41 +0300

    .

---
+++ At/Assets/Scripts/Test Scripts/HeroMove.cs
@@ -0,0 +1,105 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroMove : MonoBehaviour
+{
+    public float health = 100f;
+    public float lastHealth;
+    public float speed = 5f;
+
+    bool stopMovement;
+
+    Rigidbody2D rigHero;
+    Animator animHero;
+    SpriteRenderer srHero;
+
+    void Start()
+    {
+        rigHero = GetComponent<Rigidbody2D>();
+        animHero = GetComponent<Animator>();
+        srHero = GetComponent<SpriteRenderer>();
+
+        lastHealth = health;
+    }
+
+    void FixedUpdate()
+    {
+        if (!stopMovement)
+        {
+            rigHero.velocity = new Vector2(speed * Input.GetAxis("Horizontal"), 0f);
+        }
+    }
+    
+    void Update()
+    {
+        if (lastHealth != health)
+        {
+            animHero.SetTrigger("Hit");
+            lastHealth = health;
+
+        }
+        if (Input.GetMouseButtonDown(0))
+        {
+            health -= 5;
+        }
+        if (health <= 0)
+        {
+            animHero.SetTrigger("Dead");
+            Destroy(gameObject, 2);
+        }
+
+
+
+        if (Input.GetAxis("Horizontal") >= 0.05f)
+        {
+            srHero.flipX = false;
+            animHero.SetFloat("Speed", Input.GetAxis("Horizontal"));
+        }
+        else if (Input.GetAxis("Horizontal") <= 0.05f)
+        {
+            srHero.flipX = true;
+            animHero.SetFloat("Speed", Input.GetAxis("Horizontal") * -1);
+        }
+        else
+        {
+            animHero.SetFloat("Speed", 0f);
+        }
+
+
+
+        if (Input.GetKey(KeyCode.W) && )
+        {
+            animHero.SetBool("Jump", true);
+            stopMovement = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Attack();
+            animHero.SetTrigger("Attack");
+            animHero.SetBool("Jump", false);
+        }
+        else
+        {
+            animHero.SetBool("Jump", false);
+        }
+
+
+
+        if (animHero.GetBool("ShieldUp"))
+        {
+            stopMovement = true;
+        }
+        else
+        {
+            stopMovement = false;
+        }
+
+    }
+
+    void Attack()
+    {
+
+    }
+
+}
