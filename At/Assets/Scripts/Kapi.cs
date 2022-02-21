a28cb51dba0782a83aed06a7776d87b123789873
EmirhanSyl <61618968+EmirhanSyl@users.noreply.github.com>
2022-02-21 22:22:24 +0300

    :(

--- At/Assets/Scripts/Kapi.cs
+++ At/Assets/Scripts/Kapi.cs
@@ -7,7 +7,7 @@ public class Kapi : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Player"))
+        if (other.gameObject.CompareTag("Player"))
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         }
