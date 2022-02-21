d3c3890593c5fef5c6b83eb041baee7f1700e8ec
crnozs <ozsayincerenn@gmail.com>
2022-02-21 19:27:48 +0300

    ++++

--- At/Assets/Scripts/ExitButton.cs
+++ At/Assets/Scripts/ExitButton.cs
@@ -5,19 +5,7 @@ using UnityEngine.UI;
 
 public class ExitButton : MonoBehaviour
 {
-    public Button _button;
-
-    public void Start()
-    {
-        _button = GetComponent<Button>();
-        Invoke("interact", 7.0f);
-    }
-
-    public void interact()
-    {
-        //_button.GetComponent<Button>().inter
-    }
-
+    
     public void doExitGame()
     {
         Application.Quit();
